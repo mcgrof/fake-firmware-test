f1e0d4f73c9fd3c900829e2727f979a3d1dffafc
Luis R. Rodriguez <mcgrof@suse.com>
2015-05-05 12:04:28 -0700

    fake-firmware-test: async firmware use after free test
    This provides a test against the issue reported and
    fixed on firmware_class use of schedule_work() for
    asynchronous behaviour when taking the name of the
    desired firmware from the caller. The caller is allowed
    to pass a pointer to the firmware name, this can however
    immediately be freed by the caller and when that happens
    can cause a use-after-free scenario.
    
    In order to trigger the issue faster you can apply the
    supplied patch in the tree force-use-after-free.patch
    to your Linux kernel sources. Then just build this
    driver and load it.
    
    With a fixed kernel you should see:
    
    [  283.512445] firmware name: test_module_stuff.bin
    [  287.514020] firmware name: test_module_stuff.bin
    [  287.532489] firmware found
    
    With a buggy kernel you should see a request for arbitrary
    files but in the most common case you kernel will crash
    due to a page fault on trying to free something you cannot
    and/or NULL pointer dereference.
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@suse.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,24 @@
-all: modules
+include /lib/modules/$(shell uname -r)/build/.config
+
+ifeq ($(CONFIG_MODULE_SIG_ALL),y)
+MODSECKEY = /lib/modules/$(shell uname -r)/build/signing_key.priv
+MODPUBKEY = /lib/modules/$(shell uname -r)/build/signing_key.x509
+mod_sign_cmd = perl /lib/modules/$(shell uname -r)/build/scripts/sign-file $(CONFIG_MODULE_SIG_HASH) $(MODSECKEY) $(MODPUBKEY)
+else
+mod_sign_cmd = true
+endif
+
+export mod_sign_cmd
+
+all: modules sign_modules
+
+.PHONY: sign_modules
+sign_modules:
+	@set -e ; if [ -f $(MODPUBKEY) -a -f $(MODSECKEY) ]		;\
+		then							\
+		echo "  [SIGN] *.ko"					;\
+		$(mod_sign_cmd) *.ko					;\
+	fi
 
 .PHONY: modules
 modules:
--- test.c
+++ test.c
@@ -2,32 +2,56 @@
 #include <linux/module.h>
 #include <linux/firmware.h>
 #include <linux/platform_device.h>
-#include <linux/delay.h>
+#include <linux/slab.h>
 
 static struct platform_device *pdev;
 
+struct test_stuff {
+	char name[1024];
+};
+
+static void test_mod_cb(const struct firmware *fw, void *context)
+{
+	if (!fw) {
+		pr_info("No firmware found\n");
+	}
+	pr_info("firmware found\n");
+	release_firmware(fw);
+}
+
 static int __init test_init(void)
 {
+	struct test_stuff *stuff;
+	const char *name = "test_module_stuff.bin";
 	int ret;
-	const struct firmware *config;
 
 	pdev = platform_device_register_simple("fake-dev", 0, NULL, 0);
 	if (IS_ERR(pdev))
 		return PTR_ERR(pdev);
 
-	/* You can just do ls / > /lib/firmware/fake.bin to fake the fw */
-	ret = request_firmware(&config, "fake.bin", &pdev->dev);
-	if (ret < 0) {
-		dev_set_uevent_suppress(&pdev->dev, true);
-		platform_device_unregister(pdev);
-		return ret;
+	stuff = kzalloc(sizeof(struct test_stuff), GFP_KERNEL);
+	if (!stuff) {
+		ret = -ENOMEM;
+		goto out;
 	}
+	memcpy(stuff->name, name, strlen(name));
 
-	ssleep(3);
-
-	release_firmware(config);
+	/*
+	 * firmware worker consumer here may access stuff->name later just
+	 * too late if the driver is sloppy. For instance:
+	 * */
+	ret = request_firmware_nowait(THIS_MODULE, 1, stuff->name, &pdev->dev,
+				      GFP_KERNEL, pdev, test_mod_cb);
+	if (ret)
+		goto out;
+	kfree(stuff);
 
 	return 0;
+out:
+	dev_set_uevent_suppress(&pdev->dev, true);
+	platform_device_unregister(pdev);
+	kfree(stuff);
+	return ret;
 }
 
 static void __exit test_exit(void)
